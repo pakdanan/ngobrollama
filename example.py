fa92419b6c1592c1a69aa356076d7a0bad355443
Danan Purwantoro <danan.purwantoro@kompas.com>
2023-08-08 17:44:32 +0700

    edit

--- example.py
+++ example.py
@@ -1,24 +1,11 @@
 import streamlit as st
 import replicate
-import os
 
 # App title
 st.set_page_config(page_title="🦙💬 Llama 2 Chatbot")
 
 # Replicate Credentials
-with st.sidebar:
-    st.title('🦙💬 Llama 2 Chatbot')
-    if 'REPLICATE_API_TOKEN' in st.secrets:
-        st.success('API key already provided!', icon='✅')
-        replicate_api = st.secrets['REPLICATE_API_TOKEN']
-    else:
-        replicate_api = st.text_input('Enter Replicate API token:', type='password')
-        if not (replicate_api.startswith('r8_') and len(replicate_api)==40):
-            st.warning('Please enter your credentials!', icon='⚠️')
-        else:
-            st.success('Proceed to entering your prompt message!', icon='👉')
-    st.markdown('📖 Learn how to build this app in this [blog](https://blog.streamlit.io/how-to-build-a-llama-2-chatbot/)!')
-os.environ['REPLICATE_API_TOKEN'] = replicate_api
+replicate_api = st.text_input('Enter Replicate API token:', type='password')
 
 # Store LLM generated responses
 if "messages" not in st.session_state.keys():
